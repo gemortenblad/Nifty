da2dce3ab55eeb4c3fe05023c3d5f025e12d93eb
gemortenblad <mattias.ortenblad@gmail.com>
2015-03-29 21:56:18 +0200

    first commit

---
+++ src/Nifty/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Nifty.Startup))]
+namespace Nifty
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
